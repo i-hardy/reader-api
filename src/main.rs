add2705051c079b3338864bb999e0354cf4a0b4a
Imogen Hardy <imogen.hardy@gmail.com>
2020-02-29 17:10:04 +0000

    test: basic test setup

--- src/main.rs
+++ src/main.rs
@@ -24,9 +24,34 @@ fn index() -> &'static str {
   "Hello, world!"
 }
 
-fn main() {
+pub fn rocket() -> rocket::Rocket {
   rocket::ignite().mount("/", routes![index])
     .register(catchers![not_found])
     .attach(db::Conn::fairing())
-    .launch();
+}
+
+fn main() {
+  rocket().launch();
+}
+
+#[cfg(test)]
+mod test {
+  use super::rocket;
+  use rocket::local::Client;
+  use rocket::http::Status;
+
+  #[test]
+  fn root_path() {
+    let client = Client::new(rocket()).expect("valid rocket instance");
+    let mut response = client.get("/").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.body_string(), Some("Hello, world!".into()));
+  }
+
+  #[test]
+  fn not_found() {
+    let client = Client::new(rocket()).expect("valid rocket instance");
+    let response = client.get("/invalid-route").dispatch();
+    assert_eq!(response.status(), Status::NotFound);
+  }
 }
